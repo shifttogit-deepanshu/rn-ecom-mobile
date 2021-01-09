2eacd665310e6a19c549be0505a88b645c9bbcc4
Deepanshu Sharma <sharmadeepanshu.kaizen1@gmail.com>
2021-01-09 06:06:55 +0530

    Setup basic screen layout

---
+++ screens/shop/ProductOverviewScreen.js
@@ -0,0 +1,16 @@
+import React from "react"
+import {FlatList,StyleSheet}  from "react-natve"
+
+
+const ProductOverviewScreen = ()=>{
+    return <FlatList />
+}
+
+const styles = StyleSheet.create({
+    screen:{
+        flex:1,
+        justifyContent:"center",
+    }
+})
+
+export default ProductOverviewScreen
