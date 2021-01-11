93ca02059a97af4b09bcd60937f058f273fe8f16
Deepanshu Sharma <sharmadeepanshu.kaizen1@gmail.com>
2021-01-11 08:09:24 +0530

    Add cart Logic

--- store/reducers/cart.js
+++ store/reducers/cart.js
@@ -1,6 +1,5 @@
 import {ADD_TO_CART} from "../actions/cart"
-import CART from "../../models/cart"
-
+import {CART} from "../../models/cart"
 const initialState = {
     items: {},
     totalAmount : 0
@@ -12,10 +11,21 @@ export const cartReducer = (state=initialState,action)=>{
         case ADD_TO_CART : 
             const itemToAdd = action.productToAdd
             const id = itemToAdd.id
-            let newItemsobj = {...state.items}
-            newItemsobj[id] = itemToAdd
+            if(id in state.items){
+                const newQuantity = state.items[id].quantity+1
+                let newItemsobj = {...state.items}
+                const newPrice = itemToAdd.price*newQuantity
+                newItemsobj[id] = new CART(newQuantity,itemToAdd.price,itemToAdd.title,newPrice)
+                const total = state.totalAmount + itemToAdd.price
+                return {...state,items:newItemsobj,totalAmount:total}
             
-            return {...state,items:newItemsobj}
+            }
+            else{
+            let newItemsobj = {...state.items} 
+            const total = state.totalAmount + itemToAdd.price
+            newItemsobj[id] = new CART(1,itemToAdd.price,itemToAdd.title,itemToAdd.price)
+            return {...state,items:newItemsobj,totalAmount:total}
+            }
 
         default:
             return state
