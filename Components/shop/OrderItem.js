048c16a46f0fb813fc5eacd12944e8f776ba809b
Deepanshu Sharma <sharmadeepanshu.kaizen1@gmail.com>
2021-01-12 11:08:01 +0530

    Add order Item screen

---
+++ Components/shop/OrderItem.js
@@ -0,0 +1,20 @@
+import React from "react"
+import {View,StyleSheet,Text,Button} from "react-native"
+
+const OrderItem = (props)=>{
+    return (
+        <View>
+            <view>
+                <Text></Text>
+                <Text></Text>
+            </view>
+                <Button />
+        </View>
+    )
+}
+
+export default OrderItem
+
+const styles = StyleSheet.create({
+    
+})
