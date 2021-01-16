fcf83a3036e05ffae064ed184c6fcbc987bf436f
Deepanshu Sharma <sharmadeepanshu.kaizen1@gmail.com>
2021-01-16 13:55:25 +0530

    Add error screen

--- screens/shop/ProductOverviewScreen.js
+++ screens/shop/ProductOverviewScreen.js
@@ -1,5 +1,5 @@
 import React,{useLayoutEffect,useEffect,useState} from "react"
-import {View,FlatList,StyleSheet,Button,ActivityIndicator}  from "react-native"
+import {View,Text,FlatList,StyleSheet,Button,ActivityIndicator, RefreshControl}  from "react-native"
 import {connect} from"react-redux"
 import ProductItem from "../../Components/shop/ProductItem"
 import {HeaderButtons,Item} from 'react-navigation-header-buttons'
@@ -11,13 +11,14 @@ import { fetchProducts } from "../../store/actions/product"
 const ProductOverviewScreen = (props)=>{
 
     const [isLoading,setIsLoading] = useState(true)
+    const [refresh,setRefresh] = useState()
     const fetched = ()=>{
         setIsLoading(false)
         console.log("fetched")
     }
     useEffect(()=>{
         props.fetchProducts(fetched)
-    },[props.fetchProducts])
+    },[props.fetchProducts,refresh])
 
     useLayoutEffect(()=>{
         props.navigation.setOptions({
@@ -41,6 +42,13 @@ const ProductOverviewScreen = (props)=>{
             </View>
         )
     }
+    if(!isLoading && props.products.length==0){
+        return (
+            <View style={styles.errorScreen}>
+            <Text>No products found! Start by adding some</Text>
+             </View>
+        )
+    }
     return (
     <View style={styles.screen}>
         <FlatList data={props.products} renderItem={itemData=><ProductItem
@@ -57,6 +65,11 @@ const styles = StyleSheet.create({
     screen:{
         flex:1,
         justifyContent:"center",
+    },
+    errorScreen:{
+        flex:1,
+        justifyContent:"center",
+        alignItems:"center"
     }
 })
 
